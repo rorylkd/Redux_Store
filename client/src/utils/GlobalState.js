fc6a5784e8517d428eaf091a568b1dc8a8d3f290
rorylkd <rory.donaldson14@gmail.com>
2021-08-09 17:24:11 +0800

    Changed index.js back to how it was, changed StoreProvider in globalState.js so that it uses redux (basically what used to be in index.js)

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,33 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+// import React, { createContext, useContext } from "react";
+// import { useProductReducer } from './reducers'
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useProductReducer({
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: '',
+//   });
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
 
-export { StoreProvider, useStoreContext };
+// export { StoreProvider, useStoreContext };
+
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+export default function StoreProvider(props) {
+
+  return <Provider store={store} {...props} />;
+}
